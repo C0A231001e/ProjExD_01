d51dddc46a47a5d2b60f9c74ae7cfefd8a891b26
C0A231001e <c0a231001e@edu.teu.ac.jp>
2024-06-11 18:02:40 +0900

    ~practice6

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -10,15 +10,27 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
+    png3 = pg.image.load("fig/3.png")
+    png3 = pg.transform.flip(png3, True, False)
+    png3 = pg.transform.rotozoom(png3, 10, 1.0)
+    png3_rct = png3.get_rect()
+    png3_rct.center = 300, 200
+    
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        x = tmr % 800
+        screen.blit(bg_img, [-x, 0])
+        # screen.blit(bg_img2, [-x + 1600, 0])
+
+        screen.blit(png3, png3_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
