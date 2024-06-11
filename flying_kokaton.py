215c4e4ff8729a054bd86f24b1ee2d53a1a2649d
C0A231001e <c0a231001e@edu.teu.ac.jp>
2024-06-11 18:09:22 +0900

    practoce7

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -9,23 +9,27 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
+
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_img2 = pg.transform.flip(bg_img, True, False)
+
     png3 = pg.image.load("fig/3.png")
     png3 = pg.transform.flip(png3, True, False)
     png3 = pg.transform.rotozoom(png3, 10, 1.0)
     png3_rct = png3.get_rect()
     png3_rct.center = 300, 200
-    
+
 
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr % 800
+        x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
-        # screen.blit(bg_img2, [-x + 1600, 0])
+        screen.blit(bg_img2, [-x + 1600, 0])
+        screen.blit(bg_img, [-x + 3200, 0])
+        screen.blit(bg_img2, [-x + 4800, 0])
 
         screen.blit(png3, png3_rct)
         pg.display.update()
